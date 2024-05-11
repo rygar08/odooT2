2fddc10c420f139d7788e525370fdcc1a42fa03b
rygar08 <ryanlayton08@gmail.com>
2024-05-11 14:31:14 +1000

    odooT2 - fix kanban quick

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,6 +1,6 @@
 /** @odoo-module **/
 
-import { Component, useState , xml, onError } from "@odoo/owl";
+import { Component, useState , xml } from "@odoo/owl";
 import { Counter } from "./components/counter";
 import { Card } from "./components/card";
 
@@ -12,11 +12,13 @@ export class Playground extends Component {
         <Card title="'card 2'" content="'content of card 2'"/>
     ;`
 
+
      static components = { Counter, Card };
      setup() {
-    //    onError((e) => {
-    //      console.error(e);
-    //      alert('An error occurred. Check the console for more information.');
-    //    });
+       // onError((e) => {
+       //   console.error(e);
+       //   alert('An error occurred. Check the console for more information.');
+       // });
+
       }
 }
